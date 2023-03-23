f09835deb9aa4951454bd59a3017a959cac9305b
Riki <koops8745@gmail.com>
2023-03-22 23:04:33 -0500

    Week 3? 4?
    Animations for sprites worked out, UI elements done, will still work on more stuff as I get to it.

--- Travels in Shangshu/Assets/Player.cs
+++ Travels in Shangshu/Assets/Player.cs
@@ -1,20 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    [Header("Health")]
+    public int maxHP = 5;
+    public int currentHP;
+    
     [Header("Movement")]
     [SerializeField] private float speed = 5f;
 
     [Header("Outside Objects")]
     Rigidbody2D rb2d;
     SpriteRenderer spriteRenderer;
+    
+
+    [Header("UI Elements")]
+    public HP_Bar hpBar;
+    public Text hpText;
+    
+
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        currentHP = maxHP;
+        hpBar.setMaxHealth(maxHP);
+        hpText.text = currentHP.ToString();
     }
 
     // Update is called once per frame
@@ -39,5 +55,12 @@ public class Player : MonoBehaviour
     
     public void Stop(){
         Move(Vector3.zero);
+    }
+
+    void takeDamage(int damage)
+    {
+        currentHP -= damage;
+        hpBar.setHealth(currentHP);
+        hpText.text = currentHP.ToString();
     }
 }
